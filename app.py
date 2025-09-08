e1997a1445dac8d1f75fbb60703dba89385b5bff
gokulpmathivanan <gokulpmathivanan@gmail.com>
2025-09-08 22:08:29 +0200

    Added geocoding

--- app.py
+++ app.py
@@ -11,7 +11,15 @@ def index():
         if not city: 
             return render_template('index.html', error = "Please enter a city.")
 
-        return render_template('index.html', results = "Weather will appear here.")
+        geocode_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1&language=en&format=json"
+        geocode_response = requests.get(geocode_url)
+        if geocode_response.status_code != 200 or not geocode_response.json().get('results'):
+            return render_template('index.html', error = "City not found")
+        result = geocode_response.json()['results'][0]
+        latitude = result['latitude']
+        longitude = result['longitude']
+
+        return render_template('index.html', result = "Weather will appear here.")
 
     return render_template('index.html')
 
