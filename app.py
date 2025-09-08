6c03c2d49469b1b90552252ca037506e0d5cc77b
gokulpmathivanan <gokulpmathivanan@gmail.com>
2025-09-08 23:21:15 +0200

    Fixed port for render deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
@@ -51,5 +52,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
     
